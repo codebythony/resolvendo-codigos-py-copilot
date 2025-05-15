5c867f8abdc5fecf012c927eaff68be7c703ec52
codebythony <anthony.lima.dev@gmail.com>
2025-05-15 05:37:51 +0000

    Desafio DIO

---
+++ resolucoes_code/concat_dados.py
@@ -0,0 +1,8 @@
+# Vamos receber dois dados diferentes do usuário e concatena-los e uma string.
+
+info1 = input('Digite o primeiro dado: ')
+info2 = input('Digite o segundo dado: ')
+
+info_concatenada = info1 + " " + info2
+
+print('A informação concatenada é: ' + info_concatenada)
